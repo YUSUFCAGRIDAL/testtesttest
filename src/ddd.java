ccdedf2d6474af0861a7d497f49876cc1ffb8c41
YUSUF CAGRI DAL <yusufcagridal@gmail.com>
2022-12-08 08:35:19 +0300

    tc001 updated

---
+++ src/ddd.java
@@ -0,0 +1,20 @@
+public class ddd {
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        int k = strs[0].length();
+        for (String each : strs) {
+            if(each.length()<k){
+                k=each.length();
+            }
+        }
+        String temp="";
+        for (int i = 0; i <= k; i++) {
+            for (String each : strs) {
+                temp+=each.charAt(i);
+
+            }
+        }
+
+
+    }
+}
